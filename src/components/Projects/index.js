0656ee8d12b62e8d59eb0500c2840a4c2128c351
Chris <chris@Chriss-MacBook-Air.local>
2023-09-21 21:50:24 +1000

    updating portfolio

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -2,43 +2,57 @@ import './index.scss'
 import picCountries from '../../assets/images/countries.jpeg'
 import dreamLodge from '../../assets/images/dreamLodge.jpeg'
 import amazon from '../../assets/images/amazon.jpeg'
-import {Loader} from "react-loaders"
-
+import { Loader } from 'react-loaders'
 
 const Project = () => {
   return (
     <div className="portoflioDivs">
       <h1>Portfolio</h1>
 
-    <div className="portfolio-section-cnt">
-      <div className="portfolio-section portfolio-first">
-        <div className="portfolio-img-div">
-        <a href="https://github.com/ChrisMir26/CountriesPI" target="_blank" rel="noopener noreferrer">
-          <img src={picCountries} alt="project Pic" className="portfolio-img" />
-          </a>
+      <div className="portfolio-section-cnt">
+        <div className="portfolio-section portfolio-first">
+          <div className="portfolio-img-div">
+            <a
+              href="https://github.com/ChrisMir26/CountriesPI"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={picCountries}
+                alt="project Pic"
+                className="portfolio-img"
+              />
+            </a>
+          </div>
+        </div>
+        <div className="portfolio-section portfolio-second">
+          <div className="portfolio-img-div">
+            <a
+              href="https://dreamlodgeprueba.web.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={dreamLodge}
+                alt="project Pic"
+                className="portfolio-img"
+              />
+            </a>
+          </div>
+        </div>
+        <div className="portfolio-section portfolio-third">
+          <div className="portfolio-img-div">
+            <a
+              href="https://amazon-clone-lyart-ten.vercel.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={amazon} alt="project Pic" className="portfolio-img" />
+            </a>
+          </div>
         </div>
       </div>
-      <div className="portfolio-section portfolio-second">
-        <div className="portfolio-img-div">
-        <a href="https://dreamlodgeprueba.web.app/" target="_blank" rel="noopener noreferrer">
-
-          <img src={dreamLodge} alt="project Pic" className="portfolio-img" />
-          </a>
-        </div>
-        
-      </div>
-      <div className="portfolio-section portfolio-third">
-        <div className="portfolio-img-div">
-        <a href="https://amazon-clone-lyart-ten.vercel.app/" target="_blank" rel="noopener noreferrer">
-
-          <img src={amazon} alt="project Pic" className="portfolio-img" />
-          </a>
-        </div>
-        
-      </div>
-      </div>
-      <Loader type="pacman"/>
-
+      <Loader type="pacman" />
     </div>
   )
 }
